2bf79dcfaad94e75fa00101d8990d3c30cda0b16
Ahmed Makkawi <ahmed0makkawi@gmail.com>
2017-10-31 05:28:24 -0700

    Tutorial till video 012

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # root route
+  root 'demo#index'
+
+  # Simple Route
+  get 'demo/index'
+
+  # Default route
+  get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,57 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110000141) do
-
-  create_table "admin_users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string "first_name", limit: 25
-    t.string "last_name", limit: 50
-    t.string "email", limit: 100, default: "", null: false
-    t.string "username", limit: 25
-    t.string "hashed_password", limit: 40
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["username"], name: "index_admin_users_on_username"
-  end
-
-  create_table "contacts", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "pages", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.integer "subject_id"
-    t.string "name"
-    t.string "permalink"
-    t.integer "position"
-    t.boolean "visible", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["permalink"], name: "index_pages_on_permalink"
-    t.index ["subject_id"], name: "index_pages_on_subject_id"
-  end
-
-  create_table "sections", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.integer "page_id"
-    t.string "name"
-    t.integer "position"
-    t.boolean "visible", default: false
-    t.string "content_type"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["page_id"], name: "index_sections_on_page_id"
-  end
-
-  create_table "subjects", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string "name"
-    t.integer "position"
-    t.boolean "visible", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
